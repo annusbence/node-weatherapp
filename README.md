0cbd5bb0051bcc64e69dddc313613f6ecbbc7245
annusbence <annusbence@gmail.com>
2022-11-23 15:48:19 +0100

    Refactored and checked the code

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# node-weatherapp
+# Weather App
+
+## Built With
+  - NodeJS
+  - Express
+  - MySQL
+## Installation
+1. Clone or download the repository.
+2. Install all the dependencies using the following command.
+   ```bash
+   npm install
+   ```
+
+## APIs used
+- [OpenWeatherMap](http://openweathermap.org)
--- server.js
+++ server.js
@@ -22,68 +22,43 @@ app.get('/', (req, res) => {
 app.post('/', async (req, res) => {
   const city = req.body.city;
   const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${process.env.API_KEY}`;
+  const oldDate = new Date(Date.now() - 10 * 60 * 1000).toISOString().slice(0, 19).replace('T', ' ');
   const newDate = new Date().toISOString().slice(0, 19).replace('T', ' ');
-  const sqlSelect = `SELECT * FROM weatherapp WHERE city = '${city}' and date < '${newDate - 1000}';`
-
+  const sqlSelect = `SELECT * FROM weatherapp WHERE city = '${city}' and date > '${oldDate}';`
   connection.query(sqlSelect, async (err, rows) => {
     if (err) {
       res.sendStatus(500);
       return;
     };
-    if (rows) {
+    if (rows.length === 0) {
       const response = await fetch(url);
       const weatherData = await response.json();
-      const location = weatherData.name;
-      const longitude = weatherData.coord.lon;
-      const latitude = weatherData.coord.lat;
-      const temp = weatherData.main.temp;
-      const pressure = weatherData.main.pressure;
-      const humidity = weatherData.main.humidity;
-      const windpower = weatherData.wind.speed;
-      const winddirect = weatherData.wind.deg;
       const icon = weatherData.weather[0].icon;
-      const imgURL = `http://openweathermap.org/img/wn/${icon}@2x.png`;
 
-      const sendData = {};
-      sendData.location = location;
-      sendData.longitude = longitude;
-      sendData.latitude = latitude;
-      sendData.temp = temp;
-      sendData.pressure = pressure;
-      sendData.humidity = humidity;
-      sendData.windpower = windpower;
-      sendData.date = newDate;
-      sendData.imgURL = imgURL;
+      const sendData = {
+        longitude: weatherData.coord.lon,
+        city: weatherData.name,
+        latitude: weatherData.coord.lat,
+        temp: weatherData.main.temp,
+        pressure: weatherData.main.pressure,
+        humidity: weatherData.main.humidity,
+        windpower: weatherData.wind.speed,
+        winddirect: weatherData.wind.deg,
+        date: newDate,
+        cloud: `http://openweathermap.org/img/wn/${icon}@2x.png`
+      };
 
-      const sqlUpdate = `UPDATE weatherapp SET date='${newDate}',longitude =${longitude},latitude =${latitude},temp=${temp}, pressure=${pressure}, humidity=${humidity}, windpower=${windpower},winddirect =${winddirect}, cloud='${imgURL}' WHERE city='${city}';`;
-
-      connection.query(sqlUpdate), (err, rows) => {
+      const sqlUpdate = `UPDATE weatherapp SET ? WHERE city='${city}';`;
+      connection.query(sqlUpdate, sendData, (err, rows) => {
         if (err) {
+          console.log(err)
           res.sendStatus(500);
           return;
         }
-      }
-      res.render('index', { sendData: sendData });
-
+        res.render('index', { sendData: sendData });
+      })
     } else {
-      connection.query(sqlSelect, async (err, rows) => {
-        if (err) {
-          res.sendStatus(500);
-          return;
-        } else {
-          const dataRows = rows[0];
-          const sendData = {};
-          sendData.location = dataRows.city;
-          sendData.coordinates = dataRows.coordinates;
-          sendData.temp = dataRows.temp;
-          sendData.pressure = dataRows.pressure;
-          sendData.humidity = dataRows.humidity;
-          sendData.wind = dataRows.wind;
-          sendData.date = dataRows.date;
-          sendData.imgURL = dataRows.imgURL;
-          res.render('index', { sendData: sendData });
-        }
-      });
+      res.render('index', { sendData: rows[0] });
     }
   });
 });
